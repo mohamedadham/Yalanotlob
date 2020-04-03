8f5cf6e700442c376c51eaa7ab42ec62a7dd23fb
Kareem Morsy <kareemmorsy30@gmail.com>
2020-04-03 23:46:34 +0200

    Fix minor issue not to allow user to invite him self and only invite his friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
     def search
         query = params['input'];
-        friends = User.where(["name LIKE :query", query: "%#{query}%"]);
+        friends = User.joins(:followers).where(["name LIKE :query", query: "%#{query}%"]).where.not(id: current_user.id);
         msg = { :status => "ok", :message => friends, :html => "<b>...</b>" }
         render :json => msg # don't do msg.to_json
     end

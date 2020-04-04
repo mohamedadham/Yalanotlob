6b5d1bc4abf3a6c15eae3b188d5dd480da4bd87d
Adham <mohamedadham27@gmail.com>
2020-04-04 20:00:29 +0200

    latest orders

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :friends
   resources :groups
   resources :users
-  root to: "users#index"
+  root :to => "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

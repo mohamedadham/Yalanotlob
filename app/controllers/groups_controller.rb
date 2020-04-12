6c7efafe19972ecbb489a60e0c71cf8ee08c885f
MohamadFeeshar <mohamed.feeshar@gmail.com>
2020-04-12 09:24:34 +0200

    group can't be seen or deleted unless by the use who created it

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,21 +19,26 @@ class GroupsController < ApplicationController
         if @group.nil?
             flash[:group_error] = "No Group exists with this id"
             redirect_to :groups
+        else
+            @members = GroupMember.where(group_id: params[:id])
+            @users = @members.map{
+                |member| User.find(member.user_id)
+            }
+            @data = []
+            @data << @group
+            @data << @groups
+            @data << @users
         end
-        @members = GroupMember.where(group_id: params[:id])
-        @users = @members.map{
-            |member| User.find(member.user_id)
-        }
-        @data = []
-        @data << @group
-        @data << @groups
-        @data << @users
 
     end
 
     def destroy
-        @group = Group.find(params[:id])
+        @group = Group.find_by(id: params[:id], user_id: current_user.id)
         @group.destroy
-        redirect_to :groups
+        if @group.destroyed
+            redirect_to :groups
+        else
+            flash[:group_error] = "You can't delete this group"
+            redirect_to :groups
     end
 end
